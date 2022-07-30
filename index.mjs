88f3a535d0a1ad42767b5a4bda4363fc86eb4a4b
Jafar Kamarudeen <kamsy74@gmail.com>
2022-07-30 17:42:01 +0100

    bug fixes

--- index.mjs
+++ index.mjs
@@ -2,8 +2,8 @@ import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
 
-const accAlice = await stdlib.newTestAccounts(stdlib.parseCurrency(6000))
-const accBob = await stdlib.newTestAccounts(stdlib.parseCurrency(100));
+const accAlice = await stdlib.newTestAccount(stdlib.parseCurrency(6000))
+const accBob = await stdlib.newTestAccount(stdlib.parseCurrency(100));
 console.log('Hello, Alice and Bob!');
 
 console.log('Launching...');
@@ -12,14 +12,14 @@ const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
 const getBalance = async (who) => stdlib.formatCurrency(await stdlib.balanceOf(who));
 
-console.log(`Alice's balance before is: ${getBalance(accAlice)}`);
-console.log(`Bob's balance before is: ${getBalance(accBob)}`);
+console.log(`Alice's balance before is: ${await getBalance(accAlice)}`);
+console.log(`Bob's balance before is: ${await getBalance(accBob)}`);
 
 console.log('Starting backends...');
 
 const commonInteract = () => ({
   showTime: (t) => {
-    console.log(`Remaining time for is ${t} seconds ...`);
+    console.log(`Countdown is ${parseInt(t)} ...`);
   }
 })
 
@@ -32,10 +32,9 @@ await Promise.all([
       const choice = Math.floor(Math.random() * 2);
       const choiceMessage = !!choice ? 'I am still here!' : 'I am not here!'
       console.log(`Choice is: ${choiceMessage}`);
-      //return !!choice;
-      return true;
+      return !!choice;
     }
-    // implement Alice's interact object here
+    
   }),
   backend.Bob(ctcBob, {
     ...stdlib.hasRandom,
@@ -44,10 +43,10 @@ await Promise.all([
       console.log(`Bob has accepted the terms for a value of ${stdlib.formatCurrency(amount)}.`)
       return true;
     }
-    // implement Bob's interact object here
+    
   }),
 ]);
 
-console.log(`Alice's account balance after is: ${getBalance(accAlice)}`);
-console.log(`Bob's account balance after is: ${getBalance(accBob)}`);
+console.log(`Alice's account balance after is: ${await getBalance(accAlice)}`);
+console.log(`Bob's account balance after is: ${await getBalance(accBob)}`);
 console.log('Goodbye, Alice and Bob!');
